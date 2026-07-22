eddc7c74b313117f001ff02885e4d2e8d1232a74
Sarthak Kumar <ksarthak@Sarthaks-MacBook.local>
2026-07-22 15:55:52 +0200

    first commit - 7/22/26

---
+++ get_normal_galaxy_images.py
@@ -0,0 +1,79 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+import astropy.io.fits as fits 
+from astropy.table import Table
+from astropy.coordinates import SkyCoord
+from astropy.visualization import astropy_mpl_style, ImageNormalize, PercentileInterval, LogStretch, LinearStretch, AsinhStretch, SqrtStretch, PowerStretch
+import astropy.units as u
+
+from astroquery.esa.euclid import Euclid
+
+current_path=os.getcwd()
+gal_merger_filename=current_path+'/Q1_merger_classification.fits'
+gal_merger = Table.read(gal_merger_filename, format='fits', hdu=1)
+
+normal_mask = [
+    (gal_merger['CNN classification'] == 0) &
+    (gal_merger['CNN pred'] < 0.10)
+][0]
+
+normal_galaxies = gal_merger[normal_mask]
+
+print(f"{len(normal_galaxies)} normal galaxies found of {len(gal_merger)} total!")
+
+
+search_radius = 0.5/60
+count = 1
+cutouts = []
+
+for gal in normal_galaxies:
+    print(gal['CNN pred'], gal['CNN classification'])
+
+    ra = gal['right_ascension']
+    dec = gal['declination']
+
+    print(f"Getting normal galaxy #{count} @ RA: {ra*u.deg}, Dec: {dec*u.deg}")
+    
+    coords = SkyCoord(ra=ra*u.deg, dec=dec*u.deg, frame='icrs')
+    query = f"""
+    SELECT *
+    FROM q1.mosaic_product
+    WHERE instrument_name='VIS' AND 
+        INTERSECTS(CIRCLE({ra}, {dec}, {search_radius}), fov)=1   
+    """
+
+    res = Euclid.launch_job_async(query).get_results()
+    file_path  = f"{res['file_path'][0]}/{res['file_name'][0]}"
+    
+    cutout_out = Euclid.get_cutout(file_path=file_path, coordinate=coords, radius=4*u.arcsec, output_file='normal galaxies/gal.fits')
+    cutouts.append(cutout_out)
+
+    hdul  = fits.open(cutout_out[0])
+    image_data = hdul[0].data
+   
+    fig = plt.figure(frameon=False)
+    ax = plt.Axes(fig, [0., 0., 1., 1.])
+    ax.set_axis_off()
+    fig.add_axes(ax)
+    
+    plt.imshow(image_data, cmap='gray', origin='lower', norm=ImageNormalize(image_data, interval=PercentileInterval(98), stretch=LinearStretch()))
+    plt.savefig(f'normal galaxies/galaxy_{str(ra).replace(".", "_")}_{str(dec).replace(".", "_")}.png', bbox_inches='tight', pad_inches=0, dpi=300)
+    plt.close()
+
+    print(f"Galaxy #{count} saved to normal galaxies/galaxy_{str(ra).replace(".", "_")}_{str(dec).replace(".", "_")}.png!")
+
+    count += 1
+
+hdul  = fits.open(cutouts[0][0])
+image_data = hdul[0].data
+
+plt.figure()
+plt.title("Galaxy #1")
+
+plt.imshow(image_data, cmap='gray', origin='lower', norm=ImageNormalize(image_data, interval=PercentileInterval(98), stretch=LinearStretch()))
+colorbar = plt.colorbar()
+
+hdul.close()
+plt.show()
