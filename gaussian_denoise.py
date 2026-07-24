d89734c85608f1acf00eca65f2afe0ac7a8d53e2
Sarthak Kumar <ksarthak@Sarthaks-MacBook.local>
2026-07-24 15:00:56 +0200

    holy mother of peark

--- gaussian_denoise.py
+++ gaussian_denoise.py
@@ -4,24 +4,12 @@ import numpy as np
 from scipy import ndimage
 from astropy.visualization import ImageNormalize, PercentileInterval, LinearStretch
 
-img = Image.open('normal galaxies/galaxy63788115839234955-45912051109118394.png').convert('L').resize((80, 80))
-img_array = np.array(img)
-
-print(img.size)
-
-fig, axes = plt.subplots(2, 3, figsize=(15, 10))
-axes = axes.flatten()
-
-axes[0].imshow(img_array, cmap='gray')
-axes[0].set_title("Original")
-axes[0].axis("off")
-
 from PIL import Image
 import os
 
-folder = "normal galaxies"
-half_sigma_folder = "gaussian0_5/normal galaxies"
-full_sigma_folder = "gaussian1/normal galaxies"
+folder = "raw/anomalous galaxies"
+half_sigma_folder = "gaussian0_5/anomalous galaxies"
+full_sigma_folder = "gaussian1/anomalous galaxies"
 
 for filename in os.listdir(folder):
     if filename.lower().endswith(".png") and not os.path.isfile(os.path.join(half_sigma_folder, filename)) and not os.path.isfile(os.path.join(full_sigma_folder, filename)):
---
+++ wavelet_denoise.py
@@ -0,0 +1,33 @@
+from skimage.restoration import denoise_wavelet
+from PIL import Image
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+folder = "raw/anomalous galaxies"
+wavelet_folder = "wavelet/anomalous galaxies"
+
+for filename in os.listdir(folder):
+    if filename.lower().endswith(".png") and not os.path.isfile(os.path.join(wavelet_folder, filename)):
+        filepath = os.path.join(folder, filename)
+        
+        img = Image.open(filepath)
+        img_arr = np.array(img)
+
+        im_denoised = denoise_wavelet(
+            img_arr,
+            method='BayesShrink',
+            mode='soft',
+            wavelet_levels=3,
+            rescale_sigma=True,
+            channel_axis=None
+        )
+
+        im_denoised = (im_denoised * 255).astype(np.uint8)  # Convert back to uint8
+
+        denoised_img = Image.fromarray(im_denoised)
+        denoised_img.save(os.path.join(wavelet_folder, filename))
+
+        print(f"Processed {filename}")
+
+print("Finished denoising images.")
